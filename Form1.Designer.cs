3e543a468b72b10362c9f0c047fee6b1e80068f2
Daniel Seemer <daniel.seemer@student.kit.edu>
2017-02-06 14:09:20 +0100

    Blub

---
+++ Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pomodoro_button = new System.Windows.Forms.ToolStripMenuItem();
+            this.large_button = new System.Windows.Forms.ToolStripMenuItem();
+            this.small_button = new System.Windows.Forms.ToolStripMenuItem();
+            this.current_label = new System.Windows.Forms.ToolStripMenuItem();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.totalpomodoro_label = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.notifyIcon1.BalloonTipText = "Pomodoro";
+            this.notifyIcon1.BalloonTipTitle = "Pomodoro";
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "Pomodoro";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseClick);
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.closeToolStripMenuItem,
+            this.pomodoro_button,
+            this.large_button,
+            this.small_button,
+            this.current_label,
+            this.totalpomodoro_label});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(159, 136);
+            // 
+            // closeToolStripMenuItem
+            // 
+            this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            this.closeToolStripMenuItem.Size = new System.Drawing.Size(158, 22);
+            this.closeToolStripMenuItem.Text = "Close";
+            this.closeToolStripMenuItem.Click += new System.EventHandler(this.closeToolStripMenuItem_Click);
+            // 
+            // pomodoro_button
+            // 
+            this.pomodoro_button.Name = "pomodoro_button";
+            this.pomodoro_button.Size = new System.Drawing.Size(158, 22);
+            this.pomodoro_button.Text = "Let\'s Work";
+            this.pomodoro_button.Click += new System.EventHandler(this.pomodoro_button_Click);
+            // 
+            // large_button
+            // 
+            this.large_button.Name = "large_button";
+            this.large_button.Size = new System.Drawing.Size(158, 22);
+            this.large_button.Text = "Large Break";
+            this.large_button.Click += new System.EventHandler(this.large_button_Click);
+            // 
+            // small_button
+            // 
+            this.small_button.Name = "small_button";
+            this.small_button.Size = new System.Drawing.Size(158, 22);
+            this.small_button.Text = "Small Break";
+            this.small_button.Click += new System.EventHandler(this.makeSmallBrealToolStripMenuItem_Click);
+            // 
+            // current_label
+            // 
+            this.current_label.Enabled = false;
+            this.current_label.Name = "current_label";
+            this.current_label.Size = new System.Drawing.Size(158, 22);
+            this.current_label.Text = "Current";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(54, 44);
+            this.label1.Margin = new System.Windows.Forms.Padding(0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 31);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // totalpomodoro_label
+            // 
+            this.totalpomodoro_label.Enabled = false;
+            this.totalpomodoro_label.Name = "totalpomodoro_label";
+            this.totalpomodoro_label.Size = new System.Drawing.Size(158, 22);
+            this.totalpomodoro_label.Text = "TotalPomodoro";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Red;
+            this.ClientSize = new System.Drawing.Size(458, 121);
+            this.ControlBox = false;
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.TopMost = true;
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem pomodoro_button;
+        private System.Windows.Forms.ToolStripMenuItem large_button;
+        private System.Windows.Forms.ToolStripMenuItem small_button;
+        private System.Windows.Forms.ToolStripMenuItem current_label;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ToolStripMenuItem closeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem totalpomodoro_label;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void makeSmallBrealToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            make_short_break();
+            //contextMenuStrip1.Show(Control.MousePosition, ToolStripDropDownDirection.AboveLeft);
+            showPopup();
+        }
+
+        private void pomodoro_button_Click(object sender, EventArgs e)
+        {
+            make_pomodoro();
+            //contextMenuStrip1.Show(Control.MousePosition, ToolStripDropDownDirection.AboveLeft);
+            showPopup();
+        }
+
+        private void large_button_Click(object sender, EventArgs e)
+        {
+            make_large_break();
+            //contextMenuStrip1.Show(Control.MousePosition, ToolStripDropDownDirection.AboveLeft);
+            showPopup();
+        }
+
+        private void pause_play_button_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        const int STATE_OFF = 0;
+        const int STATE_POMODORO = 1;
+        const int STATE_LARGE = 2;
+        const int STATE_SHORT = 3;
+
+        const int LARGE_EVERY = 4;
+        const int POPUP_SEC = 10;
+
+        TimeSpan POMODORO_LENGTH = new TimeSpan(0, 25, 0);
+        TimeSpan LARGE_LENGTH = new TimeSpan(0, 15, 0);
+        TimeSpan SHORT_LENGTH = new TimeSpan(0, 5, 0);
+
+        int state = STATE_OFF;
+        DateTime current_interval_start;
+        TimeSpan current_interval_length;
+        DateTime minimize_at;
+        int num_pomodoro = 0;
+
+        TimeSpan total_pomodoro_time;
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+ 
+            switch (state) {
+                case STATE_OFF:
+                    break;
+                case STATE_POMODORO:
+                case STATE_LARGE:
+                case STATE_SHORT:
+                    if (elapsed() > current_interval_length)
+                    {
+                        next();
+                    }
+                    break;
+            }
+
+            current_label.Text = descr();
+            label1.Text = descr();
+            centerTextField();
+            this.Text = descr();
+            totalpomodoro_label.Text = "Pomodoro total: " + total_pomodoro_time.ToString(@"hh\:mm\:ss");
+            BackColor = color();
+
+            if (minimize_at < System.DateTime.Now)
+            {
+                this.WindowState = FormWindowState.Minimized;
+                this.ShowInTaskbar = false;
+            }
+        }
+
+        private void centerTextField()
+        {
+            label1.Location = new Point((this.Width - label1.Width)/2,
+                (this.Height - label1.Height)/2);
+        }
+
+        private String descr()
+        {
+            TimeSpan remaining = current_interval_length - elapsed();
+            switch (state)
+            {
+                case STATE_OFF:
+                    return "Off";
+                case STATE_POMODORO:
+                    return "Pomodoro: " + remaining.ToString(@"mm\:ss") + " left";
+                case STATE_LARGE:
+                    return "Break: " + remaining.ToString(@"mm\:ss") + " left";
+                case STATE_SHORT:
+                    return "Break: " + remaining.ToString(@"mm\:ss") + " left";
+            }
+            return "Err";
+        }
+
+        private Color color()
+        {
+            switch (state)
+            {
+                case STATE_OFF:
+                    return Color.Gray;
+                case STATE_POMODORO:
+                    return Color.Red;
+                case STATE_LARGE:
+                    return Color.LawnGreen;
+                case STATE_SHORT:
+                    return Color.LawnGreen;
+            }
+            return Color.Beige;
+        }
+
+        private void next()
+        {
+            switch (state) {
+                case STATE_OFF:
+                    make_pomodoro();
+                    break;
+                case STATE_POMODORO:
+                    if (num_pomodoro == 0)
+                    {
+                        make_large_break();
+                    } else {
+                        make_short_break();
+                    }
+                    break;
+                case STATE_LARGE:
+                    make_pomodoro();
+                    break;
+                case STATE_SHORT:
+                    make_pomodoro();
+                    break;
+            }
+
+            showPopup();
+        }
+
+        private void showPopup()
+        {
+            this.WindowState = FormWindowState.Normal;
+            this.TopMost = true;
+            minimize_at = System.DateTime.Now.AddSeconds(POPUP_SEC);
+        }
+
+        private TimeSpan elapsed()
+        {
+            return System.DateTime.Now - current_interval_start;
+        }
+
+        private void addTotalPomodoroTime()
+        {
+            if (state == STATE_POMODORO) {
+                total_pomodoro_time += System.DateTime.Now - current_interval_start;
+            }
+
+        }
+
+        private void make_pomodoro()
+        {
+            addTotalPomodoroTime();
+            current_interval_start = System.DateTime.Now;
+            num_pomodoro = (num_pomodoro + 1) % LARGE_EVERY;
+            current_interval_length = POMODORO_LENGTH;
+            state = STATE_POMODORO;
+        }
+
+        private void make_short_break()
+        {
+            addTotalPomodoroTime();
+            current_interval_start = System.DateTime.Now;
+            current_interval_length = SHORT_LENGTH;
+            state = STATE_SHORT;
+        }
+
+        private void make_large_break()
+        {
+            addTotalPomodoroTime();
+            current_interval_start = System.DateTime.Now;
+            current_interval_length = LARGE_LENGTH;
+            state = STATE_LARGE;
+        }
+
+        private void notifyIcon1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left) {
+                contextMenuStrip1.Show(Control.MousePosition, ToolStripDropDownDirection.AboveLeft);
+            }
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Minimized;
+        }
+
+        private void closeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+    }
+}
